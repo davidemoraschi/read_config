abe75c2dc7b7a02895cd57d95ceadf936e8f6673
davidemoraschi <davidem@eurostrategy.net>
2016-08-23 17:53:41 +0200

    moved to GitHub

---
+++ read_config/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace read_config
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string setting = ConfigurationManager.AppSettings["RefreshToken"];
+
+            if (setting == null)
+
+                setting = "Setting not found";
+
+            Console.WriteLine(setting);   
+        }
+    }
+}
